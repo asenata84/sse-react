b5c307c2706230de70ed6f362f712b586287e17f
Asen Tolsuzov <asen.tolsuzov@scalefocus.com>
2021-05-18 15:59:35 +0300

    Visual improvements

--- src/App.js
+++ src/App.js
@@ -2,29 +2,64 @@ import { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+const createEventSource = () => {
+  return new EventSource("http://localhost:3002/events");
+};
+
+let SSE = createEventSource();
+
 const App = () => {
   const [message, setMessage] = useState(null);
+  const [events, setEvents] = useState(SSE);
+  const [listening, setListening] = useState(false);
+
+  const logoClasses = ["App-logo"];
+  if (listening) {
+    logoClasses.push("spinning");
+  }
+
+  const startSSE = () => {
+    events.close();
+    setEvents(createEventSource());
+    setListening(true);
+  };
+
+  const stopSEE = () => {
+    setListening(false);
+    events.close();
+  };
 
   // Component did mount
   useEffect(() => {
-    const events = new EventSource("http://localhost:3002/events");
-
     // Pass event data to the state
-    events.onmessage = (e) => setMessage(JSON.parse(e.data));
+    events.onmessage = (e) => {
+      setMessage(JSON.parse(e.data));
+      setListening(true);
+    };
 
-    events.onerror = () => events.close();
+    events.onerror = () => {
+      // events.close();
+      setListening(false);
+    };
 
     return () => events.close();
-
-    // eslint-disable-next-line
-  }, []);
+  }, [events]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className={logoClasses.join(" ")} alt="logo" />
         <div>
-          Message <span className="message">{message}</span>
+          Message: <span className="message">{message}</span>
+        </div>
+        <br />
+        <div className="container">
+          <button className="App-btn" onClick={startSSE}>
+            Start events
+          </button>
+          <button className="App-btn danger" onClick={stopSEE}>
+            Stop events
+          </button>
         </div>
       </header>
     </div>
